24e171e27670e433d097007dffba40a9b175ea24
dylan-ashley <dashley@dashley.name>
2015-09-25 21:01:09 -0600

    made StatisticsHandler a proper singleton

--- app/src/main/java/ca/ualberta/dashley_reflex/StatisticsHandler.java
+++ app/src/main/java/ca/ualberta/dashley_reflex/StatisticsHandler.java
@@ -4,15 +4,12 @@ package ca.ualberta.dashley_reflex;
  * Created by dashley on 2015-09-25.
  */
 public class StatisticsHandler {
-
-    private static StatisticsHandler instance = null;
-
-    private StatisticsHandler() {};
+    private static StatisticsHandler ourInstance = new StatisticsHandler();
 
     public static StatisticsHandler getInstance() {
-        if (instance == null) {
-            instance = new StatisticsHandler();
-        }
-        return instance;
+        return ourInstance;
+    }
+
+    private StatisticsHandler() {
     }
 }
