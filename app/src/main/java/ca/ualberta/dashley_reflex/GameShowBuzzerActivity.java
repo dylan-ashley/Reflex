9117591be7023809fe7fa0e101676198a1206e29
dylan-ashley <dashley@dashley.name>
2015-10-02 15:27:55 -0600

    created switcher for GameShowBuzzerActivity

--- app/src/main/java/ca/ualberta/dashley_reflex/GameShowBuzzerActivity.java
+++ app/src/main/java/ca/ualberta/dashley_reflex/GameShowBuzzerActivity.java
@@ -1,13 +1,17 @@
 package ca.ualberta.dashley_reflex;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.NumberPicker;
 
 public class GameShowBuzzerActivity extends AppCompatActivity {
 
     private StatisticsHandler statisticsHandler = StatisticsHandler.getInstance();
+    private NumberPicker playerCountPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,10 @@ public class GameShowBuzzerActivity extends AppCompatActivity {
         if (!statisticsHandler.statisticsAreLoaded()) {
             statisticsHandler.loadFromFile(this.getBaseContext());
         }
+
+        playerCountPicker = (NumberPicker) findViewById(R.id.player_count_picker);
+        playerCountPicker.setMinValue(2);
+        playerCountPicker.setMaxValue(4);
     }
 
     @Override
@@ -39,5 +47,21 @@ public class GameShowBuzzerActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startGameShowBuzzer(View view) {
+        int playerCount = playerCountPicker.getValue();
+        if (playerCount == 2) {
+            Intent intent = new Intent(this, GameShowBuzzerTwoPlayerActivity.class);
+            startActivity(intent);
+        } else if (playerCount == 3) {
+            Intent intent = new Intent(this, GameShowBuzzerThreePlayerActivity.class);
+            startActivity(intent);
+        } else if (playerCount == 4) {
+            Intent intent = new Intent(this, GameShowBuzzerFourPlayerActivity.class);
+            startActivity(intent);
+        } else {
+            throw new RuntimeException("invalid player count received");
+        }
     }
 }
